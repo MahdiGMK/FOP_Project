2bf7ef6f48195c7a5355a7319c29246b85764cab
MahdiGmk <mbahramiand@gmail.com>
2023-01-29 14:36:01 +0330

    copy cut paste RawCommand

--- src/command_exec/RawCommand.c
+++ src/command_exec/RawCommand.c
@@ -1,6 +1,7 @@
 #include <shared/FileHandling.h>
 #include <shared/StringLib.h>
 #include "RawCommand.h"
+#include <string.h>
 
 int _CreateFile(char *address)
 {
@@ -70,4 +71,58 @@ int _RemoveStr(char *address, int l, int c, int sz, int b, int f)
     WriteFile(address + 1, file);
     LOG("File Edited Successfully");
     return 0;
+}
+char clipboard[FILESIZE];
+int _Copy(char *address, int l, int c, int sz, int f, int b)
+{
+    if (address[0] != '/' || l < 1 || c < 0 || !(b ^ f))
+        return 1;
+    char file[FILESIZE];
+    FileHandlingStatus status = ReadFileNoAlloc(address + 1, file);
+    if (status == FILE_DIDNT_EXIST)
+    {
+        LOG("File Didn't Exist");
+        return 0;
+    }
+    char *ptr = GetPtrAt(file, l, c);
+    if (b)
+        ptr -= sz;
+    strcpy(clipboard, ptr);
+    clipboard[sz] = 0;
+    return 0;
+}
+int _Cut(char *address, int l, int c, int sz, int f, int b)
+{
+    if (address[0] != '/' || l < 1 || c < 0 || !(b ^ f))
+        return 1;
+    char file[FILESIZE];
+    FileHandlingStatus status = ReadFileNoAlloc(address + 1, file);
+    if (status == FILE_DIDNT_EXIST)
+    {
+        LOG("File Didn't Exist");
+        return 0;
+    }
+    char *ptr = GetPtrAt(file, l, c);
+    if (b)
+        ptr -= sz;
+    strcpy(clipboard, ptr);
+    clipboard[sz] = 0;
+    EraseSubstring(ptr, sz);
+    WriteFile(address + 1, file);
+    return 0;
+}
+int _Paste(char *address, int l, int c, int sz, int f, int b)
+{
+    if (address[0] != '/' || l < 1 || c < 0 || !(b ^ f))
+        return 1;
+    char file[FILESIZE];
+    FileHandlingStatus status = ReadFileNoAlloc(address + 1, file);
+    if (status == FILE_DIDNT_EXIST)
+    {
+        LOG("File Didn't Exist");
+        return 0;
+    }
+    char *ptr = GetPtrAt(file, l, c);
+    InsertPattern(ptr, clipboard);
+    WriteFile(address + 1, file);
 }
