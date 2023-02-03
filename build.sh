6072f1b40b1d9a5b9fbf9d42382861378f98de70
MahdiGmk <mbahramiand@gmail.com>
2023-02-04 01:15:21 +0330

    wchar hmmmm

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 for D in src/*; do
     if [ -d "${D}" ] && [ "${D}" != "src/shared" ]; then
-        gcc "$D/"*.c "src/shared/"*.c -I "src/" -lm -lncurses -o "bin/$(basename $D).out"
+        gcc "$D/"*.c "src/shared/"*.c -I "src/" -lm -lncursesw -o "bin/$(basename $D).out"
     fi
 done
